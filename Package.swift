4509e20de4b1e5d6ecb6c18eb24a87bf96c629b8
Nabil LAHLOU <nabil.lahlou.ext@idnow.io>
2025-05-28 14:25:45 +0200

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "IDCheckIOSDK",
-            url: "https://github.com/nabil-lahlou-idnow/eid-test/releases/download/1.0.3/IDnowEID.xcframework.zip",
+            name: "IDnowEID",
+            url: "https://github.com/nabil-lahlou-idnow/eid-test/releases/download/1.0.4/IDnowEID.xcframework.zip",
             checksum: "bc0e4b96c9bbb473ecf950a9031d439f823071c311046b876441b359e2faf643"
         )
     ]
