dab82a18b9cfd70fb11356a126e0d6c4ee2d719e
Nabil LAHLOU <nabil.lahlou.ext@idnow.io>
2025-05-28 13:28:28 +0200

    Adding eid xcframework

---
+++ xcframework/IDnowEID.xcframework/ios-arm64_x86_64-simulator/IDnowEID.framework/Headers/IDnowEID.h
@@ -0,0 +1,16 @@
+//
+//  IDnowEID.h
+//  IDnowEID
+//
+//  Created by Phu Nguyen on 4/14/25.
+//  Copyright © 2025 IDnow Gmbh. All rights reserved.
+//
+
+#ifndef IDnowEID_h
+#define IDnowEID_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+#endif /* IDnowEID_h */
