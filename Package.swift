64d1c3394a61581bd1ad2b4567d361dc5807b318
Nabil Lahlou <nabil.lahlou@idnow.io>
2026-04-23 15:18:24 +0200

    Adding Open SSL

--- Package.swift
+++ Package.swift
@@ -21,31 +21,34 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", exact: "3.6.1"),
-        .package(url: "https://github.com/idnow/sunflower-sdk-ios.git", exact: "2.1.4"),
+        .package(url: "https://github.com/idnow/sunflower-sdk-ios.git", exact: "2.1.8"),
         .package(url: "https://github.com/Governikus/AusweisApp2-SDK-iOS.git", exact: "2.2.2")
     ],
     targets: [
         .binaryTarget(
             name: "IDnowEID",
-            url: "https://github.com/nabil-lahlou-idnow/eid-test/releases/download/1.0.4/IDnowEID.xcframework.zip",
-            checksum: "e723fc19003c1457eea0f40214a5dfb2f67a6e2e6a13ab9ee0c530fb8c1364ad"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.3.4/IDnowEID.xcframework.zip",
+            checksum: "2e3eeb5a9e982962ca29824599d0e21018e11384388f22404d96151ce5063853"
         ),
         .binaryTarget(
             name: "IDnowEIDDynamic",
-            url: "https://github.com/nabil-lahlou-idnow/eid-test/releases/download/1.0.4/IDnowEIDDynamic.xcframework.zip",
-            checksum: "bc3db91d8c2b289c44fbd854d2e6bedc6a51bc310d422a860c3032eba4e3b366"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.3.4/IDnowEIDDynamic.xcframework.zip",
+            checksum: "0d2675b99f73811dd6d660b41100e8552969c3133bbe82b41fd2428614ec4a9e"
         ),
         .binaryTarget(
             name: "IDnowEIDGovernikus",
-            url: "https://github.com/nabil-lahlou-idnow/eid-test/releases/download/1.0.4/IDnowEIDGovernikus.xcframework.zip",
-            checksum: "1506683a30a030a70f12d3f0f3868b1d4741ebce525fd8e1b0bd44398532acbe"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.3.4/IDnowEIDGovernikus.xcframework.zip",
+            checksum: "4321a814b4942acdcaf14dd3ba5a4700b62dab744de4b556bf566d6c00670e5c"
+        ),
+        .binaryTarget(
+            name: "OpenSSL",
+            path: "Frameworks/OpenSSL.xcframework"
         ),
         .target(
             name: "IDnowEIDDynamicWrapper",
             dependencies: [
                 "IDnowEIDDynamic",
-                .product(name: "OpenSSL", package: "OpenSSL"),
+                "OpenSSL",
                 .product(name: "SunflowerUIKit", package: "sunflower-sdk-ios")
             ],
             path: "sources-dynamic"
@@ -54,7 +57,7 @@ let package = Package(
             name: "IDnowEIDGovernikusWrapper",
             dependencies: [
                 "IDnowEIDGovernikus",
-                .product(name: "OpenSSL", package: "OpenSSL"),
+                "OpenSSL",
                 .product(name: "SunflowerUIKit", package: "sunflower-sdk-ios"),
                 .product(name: "AusweisApp2", package: "AusweisApp2-SDK-iOS")
             ],
